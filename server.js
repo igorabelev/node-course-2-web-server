98928f00a2bf178c7631348318905b5056a02505
Igor Abelev <igorabelev@Igor-MacPro.fritz.box>
2018-06-16 22:21:12 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs     = require('hbs');
+const fs      = require('fs');
+
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine','hbs');
+
+
+
+app.use((req,res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log',log + '\n', (err) => {
+    if (err) {
+        console.log('Unable to append to server.log');
+    }
+  });
+  next(); 
+});
+
+// app.use((req,res,next) => {
+//     res.render('maintenance.hbs',{
+//         pageTitle:'Maintenance Page',
+//         welcomeMessage:'The site is currently the maintenance' 
+//     });
+
+// });
+
+app.use(express.static(__dirname + '/public')); // Load static libs
+
+hbs.registerHelper('getCurrentYear',() => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+ res.render('home.hbs',{
+    pageTitle:'Welcome Page',
+    welcomeMessage:'Welcome to my Page'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs',{
+      pageTitle:'About Page'
+    });
+});
+
+
+app.get('/bad', (req,res) => {
+ 
+});
+
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+});
