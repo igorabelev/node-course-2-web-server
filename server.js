c5930d75a622dd8033ad8e29b5227f4a2348e7cf
Igor Abelev <igorabelev@Igor-MacPro.fritz.box>
2018-06-17 14:15:43 +0200

    Development of new web page: projects

--- server.js
+++ server.js
@@ -46,6 +46,13 @@ app.get('/', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs',{
+       pageTitle:'Project Page',
+       welcomeMessage:'List of my projects'
+       });
+   });
+
 app.get('/about', (req, res) => {
     res.render('about.hbs',{
       pageTitle:'About Page'
